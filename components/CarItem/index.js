c8eaff270de62aab8794f114614b900fc55c9581
Igor Povar <goshuchok@gmail.com>
2021-06-15 14:28:31 +0300

    button component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { View, Text, ImageBackground } from 'react-native';
+import StyledButton from '../StyledButton';
 import styles from './styles';
 
 const CarItem = (props) => {
   return (
     <View style={styles.carContainer}>
       <ImageBackground
-        source={require('../../assets/images/ModelX.jpeg')}
+        source={require('../../assets/images/ModelS.jpeg')}
         style={styles.image}
       />
       <View style={styles.titles}>
         <Text style={styles.title}>Model S</Text>
         <Text style={styles.subtitle}>Starting at $35,700</Text>
       </View>
+      <StyledButton />
     </View>
   );
 };
